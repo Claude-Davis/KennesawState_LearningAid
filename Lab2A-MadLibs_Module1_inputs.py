3680051e99f1014772f3f9c6f82cb67d4ec1b0d0
Claude Davis <claudesdavis16@gmail.com>
2024-11-14 23:34:12 -0500

    Lab 2 parts A and C

---
+++ Lab2A-MadLibs_Module1_inputs.py
@@ -0,0 +1,15 @@
+
+# Course CSE 1321
+# Section: BB
+# Semester: Fall 2024
+
+# prompt: file:///C:/Users/claud/AppData/Local/Temp/MicrosoftEdgeDownloads/f0d85ea3-1619-4037-8f40-070d8ed4ebcb/m2-lab-2-type-systems-v1.1.pdf
+
+#print("Lab.2.A. - MadLibs")
+
+name0 = input("Enter a name: ")
+name1 = input("Enter another name: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+
+print("Once upon a time, there was a person named", name0, "who had a child named", name1 + ". This child would eat", adverb, "while", verb, "to strangers.")
---
+++ Lab2C-RectangleCalculations_Module1_inputs-mathematics.py
@@ -0,0 +1,19 @@
+
+
+# Course CSE 1321
+# Section: BB
+# Semester: Fall 2024
+
+# prompt: file:///C:/Users/claud/AppData/Local/Temp/MicrosoftEdgeDownloads/f0d85ea3-1619-4037-8f40-070d8ed4ebcb/m2-lab-2-type-systems-v1.1.pdf
+
+#print("Lab.2.B. - Rectangle Calculations")
+
+width = int(input("Enter a width: "))
+height = int(input("Enter a height: "))
+
+area = width*height
+perimeter = (width+height)*2
+
+print("The area is", str(area))
+
+print("Thet perimeter is", str(perimeter))
