5d784c48c7a9cd72e4e0f31af5443be4d7eba41e
Claudé Davis <claudedavis2024@gmail.com>
2025-02-10 08:42:54 -0500

    Add files via upload

---
+++ Spring 2025/In-Progress/Tenant.java
@@ -0,0 +1,19 @@
+//prompt: https://www.kennesaw.edu/ccse/first-year-experience/cse1322l/assignments/cse-1322l-spring-2025-assignment-2-v1.1.pdf
+
+class Tenant {
+    //attributes
+    public String name;
+    public double rent;
+    public int age;
+
+    public Tenant(String name, int age, double rent){ //[overloaded] constructor
+        this.name = name;
+        this.age = age;
+        this.rent = rent;
+    }
+
+    //method
+    public String toString(){
+        return this.name + " (" + this.age + ") " + "| $" + this.rent + "/mo";
+    }
+}
