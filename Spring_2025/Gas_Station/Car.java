426164242c7798bb15ed68626025ff5832245c56
Claude Davis <claudesdavis16@gmail.com>
2025-02-17 11:38:00 -0500

    some updates to classes

---
+++ Spring_2025/Gas_Station/Car.java
@@ -0,0 +1,16 @@
+
+//abstract class
+abstract class Car{
+    private int id;
+    private static int nextId = 0;
+
+    public Car(){
+        this.id = nextId;
+        this.nextId++;
+    }
+
+    //getter (id)
+    public int getId(){
+        return id;
+    }
+}
--- Spring_2025/Gas_Station/GasCar.java
+++ Spring_2025/Gas_Station/GasCar.java
@@ -1,1 +1,28 @@
 //subclass of Car
+
+class GasCar extends Car{
+    //implement interface GasEngine
+    private double tankCapacity;
+    private double currentVolume;
+
+
+    public GasCar(double tankCapacity, double currentVolume){
+        this.tankCapacity = tankCapacity;
+        this.currentVolume = currentVolume;
+    }
+
+    //getter (tank capacity)
+    public double getTCapacity(){
+        return tankCapacity;
+    }
+
+    //getter (current volume)
+    public double getCVolume(){
+        return currentVolume;
+    }
+
+    @Override
+    public String toString(){
+        return "Car: " + getId() + " | Current Fuel: " + currentVolume + "/" + tankCapacity + " gallons.";
+    }
+}
