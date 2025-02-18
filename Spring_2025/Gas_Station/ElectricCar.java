d339a1f433845940c43fc14e6eab47ad64a622ec
Claudé Davis <claudedavis2024@gmail.com>
2025-02-17 19:36:39 -0500

    Update ElectricCar.java

--- Spring_2025/Gas_Station/ElectricCar.java
+++ Spring_2025/Gas_Station/ElectricCar.java
@@ -1,1 +1,17 @@
 //subclass of car
+
+class ElectricCar extends Car{
+    private double maxSpeed;
+    private double currentCharge;
+
+    class ElectricCar(double maxSpeed, double currentCharge){
+        this.maxSpeed = maxSpeed;
+        this.currentCharge = currentCharge;
+    }
+
+    @Override
+    public String toString(){
+        //
+    }
+
+}
