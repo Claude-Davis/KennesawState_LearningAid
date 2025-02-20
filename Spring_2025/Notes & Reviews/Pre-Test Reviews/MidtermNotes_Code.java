e91566f82267ac32bdec0652e83bde68f03ad94e
Claude Davis <claudesdavis16@gmail.com>
2025-02-20 13:34:10 -0500

    new folder and file w/in Notes & Reviews

---
+++ Spring_2025/Notes & Reviews/Pre-Test Reviews/MidtermNotes_Code.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class MidtermNotes_Code {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        //Converting
+            String s = "90";
+            System.out.println("This is a String: " + s);
+            //String to int
+                int i = Integer.parseInt(s);
+                System.out.println("This is now an int: " + i);
+            //String to double
+                double d = Double.parseDouble(s);
+                System.out.println("This is now a double: " + d);
+            //String to boolean
+                //
+            //String to char
+                //
+        //Converting: Tests
+            System.out.println("  int: " + s + " + 5" + " = " + (i+5));     //as an integer, the variable is added to 5
+            System.out.println("  String: " + s + " + 5" + " = " + (s+5));  //as a String, the variable is concatenated (or joined together) with 5
+            System.out.println("  double: " + s + " + 5" + " = " + (d+5));  //as a double, the variable is added to 5
+
+
+System.out.println(" --- --- ---");
+
+
+        //Casting
+        double d2 = 4.28;
+        System.out.println("Before casting, d2 = " + d2);
+        d2 = (int) 4.28;
+        System.out.println("After casting (int), d2 = " + d2);
+        d2 = (byte) 4.28;
+        System.out.println("After casting (byte), d2 = " + d2);
+
+        /* byte b = 7;
+        System.out.println("Before casting, b = " + b);
+//ERROR// b = (double) 7;
+        System.out.println("After casting (double), b = " + b); */
+
+
+System.out.println(" --- --- ---");
+
+
+        //Java vs. Python: MATH
+    }
+}
