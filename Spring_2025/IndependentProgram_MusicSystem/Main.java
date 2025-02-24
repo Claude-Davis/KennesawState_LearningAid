c24818bcfe8525df03e8a24af2903cd0e8ef6f8f
Claude Davis <claudesdavis16@gmail.com>
2025-02-24 15:05:47 -0500

    updates to MusicSystem

--- Spring_2025/IndependentProgram_MusicSystem/Main.java
+++ Spring_2025/IndependentProgram_MusicSystem/Main.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import  java.util.Scanner;
 
 public class Main{
-    public static void main (String[] args){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        ArrayList<Song> songs = new ArrayList<>();
+        ArrayList<Song> songs = new ArrayList<>();              //list to store all individually downloaded songs
+        ArrayList<Playlist> allPlaylists = new ArrayList<>();       //list to store all playlists
 
         //title
         System.out.println("{Music System}");
@@ -15,7 +16,7 @@ public class Main{
         //initialize "option"
         int option = 0;
 
-        while (option != 7){
+        while (option != 9){
             //menu
             System.out.println("1. Download a song");
             System.out.println("2. Make a playlist");
@@ -32,170 +33,174 @@ public class Main{
 
             switch(option){
                 default:
-                    System.out.println(" ");
-
                     if (option != 9)  {System.out.println("Invalid Entry");}
-                    break;
-                case 1:                                                                             // 1. download a song
-                System.out.println(" ");
-                //prompt for information
-                    System.out.println("Enter the  Song's Title: ");
-                    sc.nextLine();
-                    String title = sc.nextLine();
-                    System.out.println("Enter the Artist's Name: ");
-                    String artist = sc.nextLine();
-                    
-                //create song object
-                    Song newSong = new Song(title, artist);
-
-                //save song to "songs" list
-                    songs.add(newSong);
-
-                //statement
-                    System.out.println("New song downloaded!");
-
-                System.out.println(" ");
                 break;
+                case 1:                                                                             // 1. download a song
+                    //prompt for information
+                        System.out.print("Enter the  Song's Title: ");
+                            sc.nextLine();
+                            String title = sc.nextLine();
+                        System.out.print("Enter the Artist's Name: ");
+                            String artist = sc.nextLine();
+                        
+                    //create object of class Song
+                        Song newSong = new Song(title, artist);
 
+                    //save song to "songs" list
+                        songs.add(newSong);
+
+                    //statement
+                        System.out.println("New song downloaded!");
+
+                break;
+                case 2:                                                                             // 2. Make a playlist
+                    //prompt for information
+                        System.out.print("Name your playlist: ");
+                            sc.nextLine();
+                            String name = sc.nextLine();
+                        System.out.println("Enter a description: ");
+                            String description = sc.nextLine();
+                        System.out.println("Enter 'yes' to add songs to your playlist, or enter 'done' to move on: ");
+                        //initliaze "addSong" and ArrayList "playlist"
+                            String addSong = sc.nextLine();
+                            ArrayList<Song> playlist = new ArrayList<>();
+                            while (!addSong.equalsIgnoreCase("done")){              //NOTE: method .equalsIgnoreCase; a quicker way to compare strings and ignore differences in letter cases
+                                if (addSong.equalsIgnoreCase("yes")){
+                                    //prompt for info
+                                        System.out.print("Title: ");
+                                            //sc.nextLine();
+                                            String titleP = sc.nextLine();
+                                        System.out.print("Artist Name: ");
+                                            String artistP = sc.nextLine();
+                                    //create object of song
+                                        Song playlistSong = new Song(titleP, artistP);
+                                    //add Song object to "playlist" list
+                                        playlist.add(playlistSong);
+                                }
+                                System.out.print("Add another song? ");
+                                addSong = sc.nextLine();
+                            }
+                    
+                    //create object of class Playlist
+                        Playlist newPlaylist = new Playlist(name, description, playlist);
+                        newPlaylist.setDescription();
+
+                    //add Playlist object to "allPlaylists" ArrayList
+                        allPlaylists.add(newPlaylist);
+
+                    //statement
+                        System.out.println("New playlist created!");
+
+                break;
                 case 4:                                                                             // 4. list all songs
-                System.out.println(" ");
+                    //loop to call/print the toString() method for each Song object
+                        for (int s=0; s<=songs.size(); s++){
+                            while (s<songs.size()){
+                                System.out.println(songs.get(s).toString());
+                                s++;
+                            }
+                        }
 
-                //loop to print the toString() method for each song
-                    for (int s=0; s<=songs.size(); s++){
-                        while (s<songs.size()){
-                            System.out.println(songs.get(s).toString());
-                            s++;
+                break;
+                case 5:                                                                             // 5. list all playlists
+                    //loop to call/print toString() method for each Playlist object
+                    for (int p=0; p<=allPlaylists.size(); p++){
+                        while(p<allPlaylists.size()){
+                            System.out.println(allPlaylists.get(p).toString());
+                            p++;
                         }
                     }
-
-                System.out.println(" ");
                 break;
-
                 case 6:                                                                             // 6. search all songs
-                System.out.println(" ");
-            
                     //menu 2
-                    System.out.println("1. Search by title");
-                    System.out.println("2. Search by artist");
-                    System.out.println("3. Return to main menu");
-                    System.out.print("Choice #");
-                    int option2 = sc.nextInt();
+                        System.out.println("1. Search by title");
+                        System.out.println("2. Search by artist");
+                        System.out.println("3. Return to main menu");
+                        System.out.print("Choice #");
+                        int option2 = sc.nextInt();
             
-                    switch(option2){
-                        default:                                                         //default case
-                            System.out.println(" ");
-                            System.out.println("Invalid Entry");
+                        switch(option2){
+                            default:                                                         //default case
+                                System.out.println("Invalid Entry");
+                                break;
+                            case 1:                                                                 //search by title
+                                //prompt for song title
+                                    System.out.println("Enter song title: ");
+                                    sc.nextLine();
+                                    String songTitle = sc.nextLine();
+
+                                //search "songs" list for matching "title"
+                                    for (int t=0; t<=songs.size(); t++){
+                                        while (t<songs.size()){
+                                            if ((songs.get(t).getTitle().toLowerCase()).contains(songTitle.toLowerCase())){
+                                                System.out.println(songs.get(t).toString());
+                                            } else if (false)  {System.out.println("There are no songs with this title.");}
+                                            t++;
+                                        }
+                                    }
+
                             break;
-                        case 1:                                                                 //search by title
-                            System.out.println(" ");
-
-                            //prompt for song title
-                            System.out.println("Enter song title: ");
-                            sc.nextLine();
-                            String songTitle = sc.nextLine();
-
-                            //search "songs" list for matching "title"
-                            for (int t=0; t<=songs.size(); t++){
-                                while (t<songs.size()){
-                                    if ((songs.get(t).getTitle().toLowerCase()).contains(songTitle.toLowerCase())){
-                                        System.out.println(songs.get(t).toString());
-                                    } else if (false)  {System.out.println("There are no songs with this title.");}
-                                    t++;
-                                }
-                            }
-
-                        System.out.println(" ");
-                        break;
                         case 2:                                                                //search by artist
-                            System.out.println(" ");
-
                             //prompt for artist name to search for
-                            System.out.println("Enter artist name: ");
-                            sc.nextLine();
-                            String artistName = sc.nextLine();
+                                System.out.println("Enter artist name: ");
+                                sc.nextLine();
+                                String artistName = sc.nextLine();
 
                             //search "songs" list for matching "artist"
-                            for (int a=0; a<=songs.size(); a++){
-                                while (a<songs.size()){
-                                    if ((songs.get(a).getArtist().toLowerCase()).contains(artistName.toLowerCase())){
-                                        System.out.println(songs.get(a).toString());
-                                    } else if (false)   {System.out.println("No songs match this artist.");}
-                                    a++;
+                                for (int a=0; a<=songs.size(); a++){
+                                    while (a<songs.size()){
+                                        if ((songs.get(a).getArtist().toLowerCase()).contains(artistName.toLowerCase())){
+                                            System.out.println(songs.get(a).toString());
+                                        } else if (false)   {System.out.println("No songs match this artist.");}
+                                        a++;
+                                    }
                                 }
-                            }
 
-                        System.out.println(" ");
-                        break;
-                        case 3:  //return to main menu 
-                            System.out.println(" ");
+                            break;
+                        case 3:                                                             //return to main menu 
+
                             break;
                     }
 
-                System.out.println(" ");
                 break;
 
                 case 7:                                                              // 7. delete songs
-                System.out.println(" ");
+                    // menu 3
+                        System.out.println("1. Delete a downloaded song");
+                        System.out.println("2. Delete from a playlist");
+                        System.out.print("Choice #");
+                        int option3 = sc.nextInt();
 
-                // menu 3
-                    System.out.println("1. Delete a downloaded song");
-                    System.out.println("2. Delete from a playlist");
-                    System.out.print("Choice #");
-                    int option3 = sc.nextInt();
+                        switch(option3){
+                            default:                                                       // default case
+                                System.out.println(" ");
+                                System.out.println("Invalid Entry");
+                                break;
+                            case 1:                                                     // delete a song (id # req.)
+                                System.out.println(" ");
 
-                    switch(option3){
-                        default:                                                       // default case
-                            System.out.println(" ");
-                            System.out.println("Invalid Entry");
-                            break;
-                        case 1:
-                            System.out.println(" ");
+                                //enter id # to delete the corresponding song
+                                    System.out.print("Enter song id #: ");
+                                    int deleteSong = sc.nextInt();
 
-                            System.out.print("Enter song title: ");
-                            sc.nextLine();
-                            String deleteSong = sc.nextLine();
-
-                            //delete song from "songs" list
-                            for (int d=0; d<=songs.size(); d++){
-                                while (d<songs.size()){
-                                    if ((songs.get(d).getTitle().toLowerCase()).contains(deleteSong.toLowerCase())) {                   //find song(s) by title
-                                        System.out.println(songs.get(d).toString());
-                                    } else if (false) {
-                                        System.out.println("There are no songs with that title.");
+                                // delete song from "songs" list
+                                    for (int d=0; d<=songs.size(); d++){
+                                        if (songs.get(d).getId() == deleteSong){
+                                            songs.remove(d);
+                                        }
                                     }
-                                    d++;
-                                }
-                            }
 
-                            for (int d=0; d<=songs.size(); d++){
-                            System.out.println("Enter id # to delete: ");
-                            int deleteId = sc.nextInt();
-                                if (songs.get(d).getId() == deleteId) {                                                                                 //specify song by id #
-                                    songs.remove(d);
-                                    break;
-                                }
-                            }
+                                //statement
+                                    System.out.println("Song deleted.");
 
-                            //statement
-                                System.out.println("Song deleted.");
-                                for (int s=0; s<=songs.size(); s++){
-                                    while (s<songs.size()){
-                                        System.out.println(songs.get(s).toString());
-                                    }
-                                }
+                                System.out.println(" ");
+                                break;
+                            case 2:                    
+                                //
 
-                            System.out.println(" ");
                             break;
-                        case 2:
-                            System.out.println(" ");
+                        }
 
-                            //
-
-                            System.out.println(" ");
-                            break;
-                    }
-
-                System.out.println(" ");
                 break;
             }
         }
--- Spring_2025/IndependentProgram_MusicSystem/Playlist.java
+++ Spring_2025/IndependentProgram_MusicSystem/Playlist.java
@@ -4,14 +4,20 @@ import java.util.ArrayList;
 
 class Playlist{
     // attributes
+    private int id;
+    private static int nextId = 0;
     private String name;
-    private ArrayList<String> playlist;
-    private ArrayList<String> allPlaylists;
+    private String description;
+    private ArrayList<Song> playlist;
+    private static int playlistsCounter = 1;
 
-    public Playlist(String name, ArrayList<String> playlist){
+    public Playlist(String name, String description, ArrayList<Song> playlist){
         this.name = name;
+        this.description = description;
         this.playlist = playlist;
-        allPlaylists.add(playlist);
+        this.playlistsCounter++;
+        this.id = nextId;
+        this.nextId++;
     }
 
     //method to reward user for certain actions
@@ -21,17 +27,29 @@ class Playlist{
             System.out.println("- - Digital DJ - -");
             System.out.println("You've downloaded 10 songs!");
         }
-        if (allPlaylists.size() == 10){
+        if (playlistsCounter == 10){
             System.out.println("- - Mixtape Maestro - - ");
             System.out.println("You've made 10 playlists!");
         }
     }
 
+    //setter (description)
+    public String setDescription(){
+        if (description.equals("")){                    //if the user leaves the description empty..
+            description = "None";                           //replace it with the value "None"
+        }
+        return description;
+    }
+
     //getter (playlist)
-    public String getPlaylist()  {return playlist;}
+    public ArrayList<Song> getPlaylist()  {return playlist;}
 
     //getter (name)
     public String getPlaylistName() {return name;}
 
+    //getter (id)
+    public int getId() {return id;}
+
     //toString() method
+    public String toString()  {return "Playlist #" + id + ":  " + name + "\n   Description: " + description;}
 }
