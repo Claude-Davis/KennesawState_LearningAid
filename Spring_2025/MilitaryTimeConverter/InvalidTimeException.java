dfd2e5812e26635f608d759f072ccbc10113db9e
Claude Davis <claudesdavis16@gmail.com>
2025-03-18 09:12:53 -0400

    updates to MilitaryTimeConverter

--- Spring_2025/MilitaryTimeConverter/InvalidTimeException.java
+++ Spring_2025/MilitaryTimeConverter/InvalidTimeException.java
@@ -1,5 +1,8 @@
 package MilitaryTimeConverter;
 
-public class InvalidTimeException {
-    
+//this class is a subclass of the class "Exception"
+public class InvalidTimeException extends Exception{
+    InvalidTimeException (String x){
+        super(x);
+    }
 }
