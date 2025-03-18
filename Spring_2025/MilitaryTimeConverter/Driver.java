ac0ba5d0103b7455fb3e8bb32a5c8abae5e33978
Claude Davis <claudesdavis16@gmail.com>
2025-03-18 04:58:26 -0400

    lab8, military time converter, started

---
+++ Spring_2025/MilitaryTimeConverter/Driver.java
@@ -0,0 +1,14 @@
+package MilitaryTimeConverter;
+
+import java.util.Scanner;
+
+class Driver{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    }
+
+    //method
+    static int differenceInSeconds(String startTime, String endTime){
+        //
+    }
+}
