c0facc41a88a5478c34d7e4f2ba36f116b04ea5d
Claude Davis <claudesdavis16@gmail.com>
2025-03-24 13:38:05 -0400

    driver class added to DiffChecker

---
+++ Spring_2025/DiffChecker/Driver.java
@@ -0,0 +1,28 @@
+package DiffChecker;
+
+import java.util.Scanner;
+
+public class Driver {
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        //title
+        System.out.println("[Diff Detector]");
+
+        //prompt user for file names
+        System.out.print("Enter the name of file 1");
+        scan.nextLine();
+        String file1 = scan.nextLine();
+
+        System.out.print("Enter the name of file 1");
+        String file2 = scan.nextLine();
+
+        //end of program message
+        System.out.println(" ");
+        System.out.println("Program Complete");
+    }
+
+    static String diff(File, File){
+        //
+    }
+}
