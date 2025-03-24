59ebf0d934fa23c0bf412893b80368bd2c4b3a36
Claude Davis <claudesdavis16@gmail.com>
2025-03-23 23:19:25 -0400

    MilitaryTimeConverter code updates; near completion

--- Spring_2025/MilitaryTimeConverter/InvalidTimeException.java
+++ Spring_2025/MilitaryTimeConverter/InvalidTimeException.java
@@ -1,8 +1,12 @@
-package MilitaryTimeConverter;
+// package MilitaryTimeConverter;
+        //NOTE error: implicitly declared class should not have package declaration
 
 //this class is a subclass of the class "Exception"
 public class InvalidTimeException extends Exception{
-    InvalidTimeException (String x){
-        super(x);
+    InvalidTimeException (String message){
+        super(message);
     }
 }
+
+
+//the purpose of this class is to allow the Driver class to throw an exception for errors that are not technically illegal, but that are not desired for the user to be able to do
