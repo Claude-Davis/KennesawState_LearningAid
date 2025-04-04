52e48d6c8b7d5522a3d737e24e4200da01384e93
Claude Davis <claudesdavis16@gmail.com>
2025-04-04 13:43:02 -0400

    updates

--- Spring_2025/AlarmSystem/Alarm.java
+++ Spring_2025/AlarmSystem/Alarm.java
@@ -1,30 +1,36 @@
 package AlarmSystem;
 
-class Alarm extends Threads{
+class Alarm extends Thread{
     private int timer; //note: stored in milliseconds
     private String name;
     private int id;
     private static int nextId = 1;
 
-    public class Alarm(String name, int seconds){ 
-        if (name.equals(null)){ this.name = "Alarm X";
-        } else { this.name = name;}
-        this.timer = seconds * 1000;
+    public class Alarm(String name, int timer){
+        this.name = name;
+        this.timer = timer;
         this.id = nextId;
         nextId++;
     }
 
+    public int getTimer(){
+        return timer * 1000;
+    }
+
+    public String getName(){
+        if (name.equals("")){ return "Alarm X";}
+        else { return name; }
+    }
+
     @Override
-    public int run(){
-        this.timer -= 1000;
+    public void run(){
         try {
-            Thread.sleep(seconds * 1000)
-            if (timer == (10*1000)) { System.out.println( name + " will go off in 10 seonds."); }
+            Thread.sleep(1000);
+            timer -= 1000;
+            if (timer == (10000)) { System.out.println( name + " will go off in 10 seconds."); }
             if (timer == 0) { System.out.println(name + " has gone off."); }
         } catch (InterruptedException e) {
-            System.out.println(name + " has been interrupted at " + (timer * 1000) + " seconds.");
-        } finally {
-            Alarm.close();
+            System.out.println(name + " has been interrupted at " + (timer/1000) + " seconds.");
         }
     }
 
