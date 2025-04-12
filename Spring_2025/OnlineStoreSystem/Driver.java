ebe1290e8322058ba3bdcbf4eab46fcd969369ba
Claude Davis <claudesdavis16@gmail.com>
2025-04-12 15:04:57 -0400

    renamed

--- Spring_2025/OnlineStoreSystem/Driver.java
+++ Spring_2025/OnlineStoreSystem/Driver.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Driver2 {
+public class Driver {
     public static void main(String[] args){
         Scanner scan = new Scanner (System.in);
         Inventory inventory = new Inventory();
