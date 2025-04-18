3e6762dba058e5a701eddd1dc6c832abbb919b25
Claude Davis <claudesdavis16@gmail.com>
2025-04-18 19:57:10 -0400

    this code recieves the following error:
    Main.java:30: error: exception FileNotFoundException is never thrown in body of corresponding try statement
            } catch (FileNotFoundException e){
              ^
    1 error

--- Spring2025_FinalExamReview/RosterNGrades/Main.java
+++ Spring2025_FinalExamReview/RosterNGrades/Main.java
@@ -3,11 +3,35 @@ import java.util.Scanner;
 import java.util.NoSuchElementException;
 
 public class Main {
-    public static void main (String[] args) {
-        //
+    public static void main (String[] args) throws FileNotFoundException, NoSuchElementException{
+        System.out.println(studentGrades());
     }
 
-    public String studentGrades() {
-        //
+    public static String studentGrades() throws FileNotFoundException, NoSuchElementException{
+        File students = new File("StudentRoster.txt");
+        File grades = new File("Grades.txt");
+
+        Scanner readStudents = new Scanner(students);
+        Scanner readGrades = new Scanner(grades);
+        
+        String studentsName;
+        String grade;
+
+        StringBuilder lines = null;
+
+        try{
+            while (readStudents.hasNextLine()) {
+                studentsName = readStudents.nextLine();
+                grade = readGrades.nextLine();
+
+                lines.append("\n" + studentsName +": " + grade);
+            }
+            return lines.toString();
+        } catch (FileNotFoundException e){
+            System.out.println("ERROR: " + e.getMessage());
+        } catch (NoSuchElementException e){
+            System.out.println("ERROR: " + e.getMessage());
+        }
+        return lines.toString();
     }
 }
