bebea9dc5fc89f500a5e49be1bde70f0e4c2682b
Claude Davis <claudesdavis16@gmail.com>
2025-04-18 17:53:27 -0400

    updates to calling diff() method

--- Spring2025_FinalExamReview/DiffChecker/DiffDriver.java
+++ Spring2025_FinalExamReview/DiffChecker/DiffDriver.java
@@ -1,16 +1,18 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Driver {
+public class DiffDriver {
     public static String diff(File file1, File file2) throws FileNotFoundException{
+        String line1 = "";
+        String line2 = "";
+
+        int lineNum = 0;
+
+        String printThis = " ";
+
         try {
             Scanner forTest1 = new Scanner(file1);
             Scanner forTest2 = new Scanner(file2);
-
-            String line1 = "";
-            String line2 = "";
-
-            int lineNum = 0;
 
             while ((forTest1.hasNextLine()) || (forTest2.hasNextLine())) {
                 line1 = forTest1.nextLine();
@@ -20,34 +22,40 @@ public class Driver {
 
                 //line differences
                 if (!line1.equals(line2)){
-                    System.out.println("Line " + lineNum +": \n< " + line1 + "\n> " + line2);
+                    printThis = "Line " + lineNum +": \n< " + line1 + "\n> " + line2;
                 }
 
                 //number of line differences
                 if ((forTest1.hasNextLine() && !forTest2.hasNextLine()) || (forTest2.hasNextLine() && !forTest1.hasNextLine())) {
-                    System.out.println("Files have different number of lines.");
+                    printThis = "Files have different number of lines.";
                 }
             }
 
             //line identical
             if (line1 == line2) {
-                System.out.println("Files are identical.");
+                printThis = "Files are identical.";
             }
         } catch (FileNotFoundException e) {
             System.out.println("Error: " + e.getMessage());
         }
+
+        //printThis = printThis;
+        return printThis;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
         Scanner scan = new Scanner(System.in);
         System.out.println("[Diff Detector]");
         int opt = 0;
 
-        while (opt!=8){
-            //menu
-            switch (opt){
-                //
-            }
-        }
+            System.out.print("Enter the name of file 1: ");
+                String fileName1 = scan.nextLine();
+            System.out.print("Enter the name of file 2: ");
+                String fileName2 = scan.nextLine();
+
+            File file1 = new File(fileName1);
+            File file2 = new File(fileName2);
+
+            System.out.println(diff(file1, file2));
     }
 }
