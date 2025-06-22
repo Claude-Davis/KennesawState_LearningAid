739c6391e5c340a53292b18f9f6d57915224260d
Claude Davis <claudedavis2024@gmail.com>
2025-06-22 05:47:27 -0400

    lab 5 completed

--- Summer_2025/Lab/Lab5.py
+++ Summer_2025/Lab/Lab5.py
@@ -7,5 +7,17 @@
 # Resources:
 
 
+def is_prime(num):
+    if num == 1: # ensures that an input of 1 makes the function False
+        return False
+    for i in range(2, num): # divides the given number by all numbers from 2 to num-1
+        if num % i == 0: # if the quotient is not a whole number/if it has a remainder, the functoin is False (not prime)
+            return False
+    return True
 
+num = int(input("Enter Number: "))
 
+if is_prime(num):
+    print(str(num) + " is prime")
+else:
+    print(str(num) + " is not prime")
