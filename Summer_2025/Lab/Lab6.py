f1c525e9f2eb59f1badfac2ff8494e5baa476c4a
Claude Davis <claudedavis2024@gmail.com>
2025-06-22 07:03:52 -0400

    lab 6

---
+++ Summer_2025/Lab/Lab6.py
@@ -0,0 +1,54 @@
+# Starlan Davis
+# Lab #6
+# Due: 24 June 2025
+# Purpose: a class intended to be utilized in future labs
+# Resources:
+
+
+class Worker:
+    def __init__(self):
+        self.employeeNum = 0
+        self.officeNum = 0
+        self.name = "First Last"
+        self.birthdate = "00/00/0000"
+        self.hoursTotal = 0
+        self.overtimeTotal = 0
+
+
+    def get_employee_number(self):
+        return self.employeeNum
+    def set_employee_number(self, x):
+        self.employeeNum = x
+
+    def get_office_number(self):
+        return self.officeNum
+    def set_office_number(self, x):
+        self.officeNum = x
+        if (self.officeNum <100) or (self.officeNum>500):
+            return False
+        return True
+
+
+    def get_name(self):
+        return self.name
+    def set_name(self, x):
+        self.name = x
+
+    def set_birthdate(self, d, m, y):
+        self.birthdate = str(d)+"/"+str(m)+"/"+str(y)
+        if (1>d>31):
+            return False
+        if (1>m>12):
+            return False
+        return True
+
+    def get_hours_worked(self):
+        return self.hoursTotal
+    def add_hours(self, x):
+        if (x<=9):
+            self.hours += x
+        elif (x>9):
+            self.hours += 9
+            self.overtimeTotal += (x-9)
+    def get_hours_overtime(self):
+        return self.overtimeTotal
--- Summer_2025/Lab/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# Lab4.py
-# IT1114/Section W01
-# Starlan Davis
-# Lab #4
-# Due: 17 June 2025
