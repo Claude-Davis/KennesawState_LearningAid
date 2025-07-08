8220182ea25d902a027a4649856d01bd8da4920e
Claude Davis <claudedavis2024@gmail.com>
2025-07-08 05:24:51 -0400

    code reqs completed

--- Summer_2025/Lab/lab9.py
+++ Summer_2025/Lab/lab9.py
@@ -2,32 +2,61 @@
 # Lab #9
 # Due: 8 July 2025
 # Purpose: program for password verification
-# Resources:
+# Resources: used github copilot to learn/remind myself of certain ethods (ex: isdigit())
 
-lower_case_char = False
-upper_case_char = False
-def casing_check(contrasena):
-    for a in contrasena:
-        if a.islower():
-            lower_case_char = True
-            break
+def capsCasing_check(contrasena):
+    upper_case_char = False
     for A in contrasena:
         if A.isupper():
             upper_case_char = True
-            break
-
-number_present = False
+            return upper_case_char
+        
+def lowerCasing_check(contrasena):
+    lower_case_char = False
+    for a in contrasena:
+        if a.islower():
+            lower_case_char = True
+            return lower_case_char
+    
 def number_check(contrasena):
+    number_present = False
     for n in contrasena:
-        print(x)
+        if n.isdigit():
+            number_present = True
+            return number_present
+        
+def specialChar_check(contrasena):
+    specialChar_present = False
+    for s in contrasena:
+        if (s == "!") or (s == "@") or (s == "#") or (s == "$") or (s == "%") or (s == "/"):
+            specialChar_present = True
+            return specialChar_present
+        elif (s == "*") or (s == "(") or (s == ")") or (s == "-") or (s == "_"):
+            specialChar_present = True
+            return specialChar_present
+
 
 
 password = input("Password: ")
+lower_case_char = lowerCasing_check(password)
+upper_case_char = capsCasing_check(password)
+number_present = number_check(password)
+specialChar_present = specialChar_check(password)
 
 if (len(password) < 9):  # verify length of password
     print("Invalid Password")
-else:
+
+elif (len(password)>=9):
     if ((not lower_case_char) or (not upper_case_char)):   # verify presence of lower and upper case letters
         print("Invalid Password")
-    else:
-        //
+
+    elif (lower_case_char and upper_case_char):  
+        if(not number_present):     # very presence of a number
+            print("Invalid Password")
+
+        elif (number_present):
+            if (not specialChar_present):   # verify presence of a special character
+                print("Invalid Password")
+
+            elif (specialChar_present):
+                print("Valid Password")
